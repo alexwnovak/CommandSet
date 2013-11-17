4d1737802de827eed4041146db1e3759be97179a
Alex Novak <alexwnovak@gmail.com>
2013-11-17 18:55:27 -0500

    +AppControllerTest and initial test for returning 0.

---
+++ CommandSet/UpCommand.UnitTest/AppControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommandSet.UpCommand.UnitTest
+{
+   [TestClass]
+   public class AppControllerTest
+   {
+      [TestMethod]
+      public void Run_NullArguments_ReturnsExitCodeZero()
+      {
+         var appController = new AppController();
+
+         int exitCode = appController.Run( null );
+
+         Assert.AreEqual( 0, exitCode );
+      }
+   }
+}
