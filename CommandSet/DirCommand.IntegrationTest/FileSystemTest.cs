db6899e4d3d63cae8b8214046e8518dc04e8e0c9
Alex Novak <alexwnovak@gmail.com>
2013-11-16 22:56:03 -0500

    Migrating integration test classes.

---
+++ CommandSet/DirCommand.IntegrationTest/FileSystemTest.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommandSet.DirCommand.IntegrationTest
+{
+   [TestClass]
+   public class FileSystemTest
+   {
+      [TestInitialize]
+      public void Initialize()
+      {
+         IntegrationHelper.DeleteTempDirectory();
+      }
+
+      [TestMethod]
+      [TestCategory( "Integration" )]
+      public void GetFiles_DirectoryIsEmpty_ReturnsZeroFiles()
+      {
+         // Setup
+
+         string tempDirectory = IntegrationHelper.CreateTempDirectory();
+
+         // Test
+
+         var fileSystem = new FileSystem();
+
+         var files = fileSystem.GetFiles( tempDirectory );
+
+         Assert.AreEqual( 0, files.Count() );
+      }
+
+      [TestMethod]
+      [TestCategory( "Integration" )]
+      public void GetFiles_DirectoryHasOneFile_ReturnsTheOneFile()
+      {
+         // Setup
+
+         string tempDirectory = IntegrationHelper.CreateTempDirectory();
+         string fileName = IntegrationHelper.CreateTempFile( tempDirectory );
+
+         // Test
+
+         var fileSystem = new FileSystem();
+
+         var files = fileSystem.GetFiles( tempDirectory );
+
+         Assert.AreEqual( 1, files.Count() );
+         Assert.AreEqual( fileName, files.First().FullName );
+      }
+
+      [TestMethod]
+      [TestCategory( "Integration" )]
+      public void GetFiles_DirectoryHasOneFileAndOneSubDirectory_ReturnsAll()
+      {
+         // Setup
+
+         string tempDirectory = IntegrationHelper.CreateTempDirectory();
+         string subDirectory = IntegrationHelper.CreateSubDirectory( tempDirectory );
+         string fileName = IntegrationHelper.CreateTempFile( tempDirectory );
+
+         string fullDirectoryPath = Path.Combine( tempDirectory, subDirectory );
+         string fullPath = Path.Combine( tempDirectory, subDirectory, fileName );
+
+         // Test
+
+         var fileSystem = new FileSystem();
+
+         var files = fileSystem.GetFiles( tempDirectory );
+
+         Assert.AreEqual( 2, files.Count() );
+         Assert.AreEqual( fullDirectoryPath, files.First().FullName );
+         Assert.AreEqual( fullPath, files.ElementAt( 1 ).FullName );
+      }
+
+      [TestMethod]
+      [TestCategory( "Integration" )]
+      public void GetFiles_DirectoryHasOneFile_ReturnsFileSize()
+      {
+         const int fileSize = 12;
+
+         // Setup
+
+         string tempDirectory = IntegrationHelper.CreateTempDirectory();
+         IntegrationHelper.CreateTempFile( tempDirectory, fileSize );
+
+         // Test
+
+         var fileSystem = new FileSystem();
+
+         var files = fileSystem.GetFiles( tempDirectory );
+
+         // Assert
+
+         Assert.AreEqual( fileSize, files[0].Length );
+      }
+
+      [TestMethod]
+      public void GetFiles_HasDirectory_ReturnsDirectoryFlagSet()
+      {
+         // Setup
+
+         string tempDirectory = IntegrationHelper.CreateTempDirectory();
+         const string subDirectory = "OneSubDirectory";
+         string fullPath = Path.Combine( tempDirectory, subDirectory );
+         Directory.CreateDirectory( fullPath );
+
+         // Test
+
+         var fileSystem = new FileSystem();
+
+         var files = fileSystem.GetFiles( tempDirectory );
+
+         // Assert
+
+         Assert.AreEqual( 1, files.Length );
+         Assert.AreEqual( fullPath, files[0].FullName );
+         Assert.IsTrue( files[0].IsDirectory );
+      }
+   }
+}
---
+++ CommandSet/DirCommand.IntegrationTest/IntegrationHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CommandSet.DirCommand.IntegrationTest
+{
+   public static class IntegrationHelper
+   {
+      private const string _testDirectoryName = "DirCommand_IntegrationTest";
+
+      public static string CreateTempDirectory()
+      {
+         string tempPath = Path.GetTempPath();
+         string testDirectory = Path.Combine( tempPath, _testDirectoryName );
+         string runDirectory = GetRunDirectoryName() + GetNextIndex( testDirectory );
+
+         string finalPath = Path.Combine( tempPath, _testDirectoryName, runDirectory );
+
+         Directory.CreateDirectory( finalPath );
+
+         return finalPath;
+      }
+
+      private static string GetRunDirectoryName()
+      {
+         return DateTime.Now.ToString( "yyyy-MM-d" ) + "_Run";
+      }
+
+      private static int GetNextIndex( string path )
+      {
+         string thisDirectory;
+         int index = 0;
+
+         do
+         {
+            index++;
+            thisDirectory = GetRunDirectoryName() + index;
+         }
+         while ( Directory.Exists( Path.Combine( path, thisDirectory ) ) );
+
+         return index;
+      }
+
+      public static string CreateSubDirectory( string tempDirectory )
+      {
+         string directoryName = "TempDirectory_" + Guid.NewGuid();
+
+         string fullPath = Path.Combine( tempDirectory, directoryName );
+
+         Directory.CreateDirectory( fullPath );
+
+         return directoryName;
+      }
+
+      public static void DeleteTempDirectory()
+      {
+         string tempPath = Path.GetTempPath();
+
+         string testDirectory = Path.Combine( tempPath, _testDirectoryName );
+
+         try
+         {
+            Directory.Delete( testDirectory, true );
+         }
+         catch ( DirectoryNotFoundException )
+         {
+            // It's OK to eat this exception. Attempting to delete an already-deleted item counts as
+            // a "no op," since we got what we want: the directory is gone
+         }
+      }
+
+      public static string CreateTempFile( string tempDirectory, int length = 0 )
+      {
+         string fileName = "TempFile_" + Guid.NewGuid();
+
+         string fullPath = Path.Combine( tempDirectory, fileName );
+
+         using ( var fileStream = File.Create( fullPath ) )
+         {
+            if ( length != 0 )
+            {
+               byte[] bytes = Enumerable.Repeat( (byte) '#', length ).ToArray();
+
+               fileStream.Write( bytes, 0, bytes.Length );
+            }
+         }
+
+         return fullPath;
+      }
+   }
+}
