04532f277b71e4aac519333da21cdeb4067fdda7
Alex Novak <alexwnovak@gmail.com>
2013-11-18 12:50:08 -0500

    +DataExchangeService blank service project.

---
+++ CommandSet/DataExchangeService/MainService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace CommandSet.DataExchangeService
+{
+   public partial class MainService : ServiceBase
+   {
+      public MainService()
+      {
+         InitializeComponent();
+      }
+
+      protected override void OnStart( string[] args )
+      {
+      }
+
+      protected override void OnStop()
+      {
+      }
+   }
+}
