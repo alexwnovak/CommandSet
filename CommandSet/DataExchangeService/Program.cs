1629ceabbde9066dd4be50b8b4c86eba8f172c8d
Alex Novak <alexwnovak@gmail.com>
2013-11-18 21:39:36 -0500

    +ServerApi concrete + DI hookup in entry point.

--- CommandSet/DataExchangeService/Program.cs
+++ CommandSet/DataExchangeService/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System.ServiceProcess;
+using CommandSet.DependencyServices;
 
 namespace CommandSet.DataExchangeService
 {
    internal static class Program
    {
+      private static void InitDependencyInjection()
+      {
+         Dependency.CreateUnityContainer();
+         Dependency.RegisterType<IServerApi, ServerApi>();
+      }
+
       /// <summary>
       /// The main entry point for the application.
       /// </summary>
       private static void Main()
       {
+         InitDependencyInjection();
+
          var services = new ServiceBase[] 
          { 
             new MainService() 
---
+++ CommandSet/DataExchangeService/ServerApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommandSet.DataExchangeService
+{
+   public class ServerApi : IServerApi
+   {
+      public void Start()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Stop()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
