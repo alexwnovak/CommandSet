d581313b10f5ba7bc7749d82785604b08a05e171
Alex Novak <alexwnovak@gmail.com>
2013-11-18 20:52:53 -0500

    +ServiceInstaller with name and description.

---
+++ CommandSet/DataExchangeService.UnitTest/MainServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommandSet.DataExchangeService.UnitTest
+{
+   [TestClass]
+   public class MainServiceTest
+   {
+      [TestMethod]
+      public void OnStart_()
+      {
+         var mainService = new MainService();
+
+         
+      }
+   }
+}
